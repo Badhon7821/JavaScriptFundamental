4de1906c0ccb8666065554ee3760530713615632
Badhon7821 <bayozidbostamibadhon@gmail.com>
2022-08-25 10:02:06 -0700

    Fundamental-End

--- Fundamental-Part-02/script.js
+++ Fundamental-Part-02/script.js
@@ -1,4 +1,203 @@
 
+console.log("Hello World");
+console.log("Hi Badhon");
+
+//variables
+let herNAme = "Sharmin";
+let myName = "Badhon";
+console.log(herNAme);
+//two string adding
+console.log(herNAme +" " + myName)
+// camel case : herName, heIsRani
+let movieRating = 9;
+console.log(movieRating);
+
+
+/*  Data type : Data Type is two type that is Object type and Primitive type
+Primitive type are 7 type that is
+1. Number(integer and floating)  // let herAge= 10; let herAge = 10.5;
+2. String // let herName = "Rokeya";
+3. Boolean // let herAge = true; let herAge = false;
+4. Undefined // let herAge = " ";
+5. Null
+6. Symbol
+7.bigInt */
+
+
+//Mathmatical term
+const myBirthYear = 1999;
+const herBirthYear = 2000;
+const birthYearDistance = myBirthYear - herBirthYear;
+const birthYearAddition = myBirthYear + herBirthYear;
+const birthYearMultiplication = myBirthYear * herBirthYear;
+const birthYearDivision = myBirthYear / herBirthYear;
+console.log(birthYearDistance);
+console.log(birthYearAddition);
+console.log(birthYearMultiplication);
+console.log(birthYearDivision);
+
+//Long Sentence Writing
+console.log("I am a student"+ " " + "My age 10");
+let number = 10;
+ number += 10;
+ number -= 10;
+ number *= 10;
+ number /= 10;
+ console.log(number);
+
+ //Backtique javascript or template leteratue
+ const bioData = `I am ${myName} and he is ${herNAme}.My birth year ${myBirthYear} and her birth year ${herBirthYear}`;
+ console.log(bioData);
+
+ //If else conditin
+const bscPassYear = 2025;
+const marriedPossiblity = 80;
+if(bscPassYear<=2024)
+{
+    console.log(`${marriedPossiblity}%`);
+}
+else{
+    console.log(`${100 - marriedPossiblity}%`);
+}
+// if, else if, else condition
+// grade define
+const herMark = 80;
+if(herMark<=33){
+    console.log("fail");
+}else if(herMark>=33 && herMark<=60){
+    console.log("A");
+}else{
+    console.log("A+");
+}
+
+// string + number and number + string data type automatically change so this is Coercion
+const myAge = "23";
+console.log(myAge+ 20);
+console.log(myAge-10);
+const herAge = 24;
+console.log("23" + herAge);
+console.log("42"- herAge);
+
+// string to number & number to string data type conversion is called data type conversion
+const testNumber = 12;
+const testNumberToString = String(testNumber);
+console.log(testNumberToString);
+console.log(String(12));
+console.log(Number("20"));
+
+
+// Checking String Data Type 
+console.log(typeof myAge);
+console.log(typeof NaN); //This is JS bug cause Undefined but this string type Number
+
+// Leap Year test
+/* const year = Number(prompt("Enter choice your year : "));
+
+if(year){
+    if(year%400 === 0 || (year % 4 === 0 && year % 100 !== 0)){
+        console.log(`${year} is leap year `);
+    }
+    else{
+        console.log(`${year} is no leap year`);
+    }
+}
+else{
+    const year = Number(prompt("Enter a valid year"));
+    if(year%400 === 0 || (year % 4 === 0 && year % 100 !== 0)){
+        console.log(`${year} is leap year `);
+    }
+    else{
+        console.log(`${year} is no leap year`);
+    }
+} */
+
+//  Switch Case statement---Digit define
+
+/* const digit = Number(prompt("Enter Choice Digit : "));
+
+if(digit){
+    switch(digit){
+        case 0:
+            console.log("Zero");
+            break;
+        case 1:
+            console.log("One");
+            break;
+        case 2:
+            console.log("Two");
+            break;
+        case 3:
+            console.log("Three");
+            break;
+        case 4:
+            console.log("Four");
+            break;
+        case 5:
+            console.log("Five");
+            break;
+        case 6:
+            console.log("Six");
+            break;
+        case 7:
+            console.log("Seven");
+            break;
+        case 8:
+            console.log("Eight");
+            break;
+            default:
+            console.log("Nine");
+    }    
+}
+else{
+    const digit = Number(prompt("Enter a valid Digit : "));
+    switch(digit){
+        case 0:
+            console.log("Zero");
+            break;
+        case 1:
+            console.log("One");
+            break;
+        case 2:
+            console.log("Two");
+            break;
+        case 3:
+            console.log("Three");
+            break;
+        case 4:
+            console.log("Four");
+            break;
+        case 5:
+            console.log("Five");
+            break;
+        case 6:
+            console.log("Six");
+            break;
+        case 7:
+            console.log("Seven");
+            break;
+        case 8:
+            console.log("Eight");
+            break;
+            default:
+            console.log("Nine");
+    } 
+}
+ */
+
+// Boolean Data Type check
+// Falsy value - 0 ,"", null, undefined, NaN
+// Truth value - " ", 1, {}-this is object
+console.log(Boolean(0)); //false
+console.log(Boolean(" ")); // true
+console.log(Boolean("")); // False
+console.log(Boolean(null)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean(NaN)); // false
+console.log(Boolean(1)); // true
+console.log(Boolean({})); // true
+
+
+
 //  Function Declaration
 
 function temperatureInRoom(temp){
